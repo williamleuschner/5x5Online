b192d5ee265cd6a7d6f4fa0398ad86cf0a62888e
William Leuschner <iPlayGames2012@live.com>
2014-04-30 23:51:55 -0400

    Preliminary AJAX, polish, banner
    Added preliminary AJAX handler. Will probably be changed. Added JS to
    display a banner notification. Borrowed from pattern.dk/sun. Polished
    assorted code.

--- script/main.js
+++ script/main.js
@@ -11,6 +11,8 @@ $$('.panel-left').on('open', function() {
 });
 five.params["modalTitle"] = '5x5 Online';
 var connected = false;
+var modalCallbackFinished = false;
+var userDidSkipYesNo = false;
 function setTimeField() {
 	//Make a date
 	var d = new Date();
@@ -101,7 +103,7 @@ function getPeriodDebug(testDate) {
 }*/
 function submitCheck() {
 	//Checks the submission before it is submitted.
-	//Success variable
+	//Success variable for yes/no box
 	//text boxen
 	var name = document.getElementById("teacherName").value;
 	var subject = document.getElementById("subject").value;
@@ -154,39 +156,52 @@ function submitCheck() {
 	}
 	//Did the user check any of the checkboxes?
 	if ( !(b["rulesPosted"] || b["teacherMobile"] || b["appropriateTone"] || b["usedPraise"] || b["usedMotivation"] || b["conseqPosted"] || b["positiveRapport"] || b["diffInstruction"] || b["activeStudentPart"] || b["collabLearnStrat"] || b["activationStrat"] || b["summStrat"] || b["criticalThinking"] || b["creating"] || b["evaluating"] || b["analyzing"] || b["applying"] || b["understanding"] || b["remembering"] || b["essays"] || b["oeQuestions"] || b["lessonDrivenPrompts"]) ) {
-			five.modal({
-				title:"Are You Sure?",
-				text:"You have not selected any checkboxes. Are you sure you want to sumbit?",
-				afterText: "",
-				buttons:[{
-					text:"No",
-					bold: true,
-					close: true
-				},{
-					text:"Yes",
-					bold: false,
-					onClick: handleData(name),
-					close: false
-				}]
-			});
-			return;
-		}
-	var nameSplit = name.split(", ");
-	var email = findTeacher(nameSplit[0], nameSplit[1]);
-	if (email == false || email == undefined) {
+		five.modal({
+			title:"Are You Sure?",
+			text:"You have not selected any checkboxes. Are you sure you want to sumbit?",
+			afterText: "",
+			buttons:[{
+				text:"No",
+				bold: true,
+				close: true
+			},{
+				text:"Yes",
+				bold: false,
+				onClick: yesNoHandler1(),
+				close: false
+			}]
+		});
+	}
+	/*if (email == false || email == undefined) {
 		five.alert('It appears as though that teacher doesn\'t exist. Did you misspell their name?', 'Error');
 		return;
+	}*/
+	handleData(name, subject, time, b, behaviors, adminComments, ponder)
+}
+function handleData(name, subject, time, b, behaviors, adminComments, ponder) {
+	console.log("Handle function called.");
+	while (!modalCallbackFinished) {
+		console.log("Waiting for user...");
 	}
-	handleData(name, subject, time, email, b, behaviors, adminComments, ponder)
+	if (!userDidSkipYesNo) {
+		return;
+	}
+	bJSON = JSON.stringify(b);
+	behaviorsJSON = JSON.stringify(behaviors);
+	var submitAjax = new XMLHttpRequest();
+	submitAjax.onreadystatechange = function() {
+		if (xmlhttp.readyState == 4 && xmlhttp.status == 200) {
+			five.alert(submitAjax.responseText);
+		}
+	}
+	submitAjax.open("POST", "/5x5Online/send5x5", true);
+	submitAjax.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
+	submitAjax.send("name=" + name + "&subject=" + subject + "&time=" + time + "&adminComments=" + adminComments + "&ponder=" + ponder + "&b=" + bJSON + "&behaviors=" + behaviorsJSON);
 }
-function handleData(name, subject, time, email, b, behaviors, adminComments, ponder) {
-	five.alert("Email: " + email, "Debug")
-}
-function findTeacher(last, first) {
-	//Find the teacher specified.
-	//Returns false if no teacher found.
-	//Returns email address if found.
-	return "fgleuschner@cdschools.org";
+function yesNoHandler1(userDidSkip) {
+	five.closeModal();
+	userDidSkipYesNo = userDidSkip;
+	modalCallbackFinished = true;
 }
 function save5x5() {
 	five.alert("I haven't written this yet.");
@@ -197,9 +212,41 @@ function load5x5() {
 function connectionState(bool) {
 	if (bool) {
 		connected = true;
+		banner("Internet connected.");
 	} else {
 		connected = false;
+		banner("Internet disconnected.");
 	}
+}
+function banner(text) {
+    if ($$('#banner').length > 0) {
+        $$('#banner').remove();
+        clearTimeout(timeout);
+    }
+
+    $$('.views').addClass('reduce');
+
+    var notification = document.createElement('div');
+    notification.id = "banner"
+    notification.innerHTML = text;
+    $$('body').prepend(notification);
+
+    notification = $$("#banner");
+    timeout = setTimeout(function () {
+        notification.addClass('close');
+        $$('.views').removeClass('reduce');
+        setTimeout(function() {
+            notification.remove();
+        }, 300 );
+    }, 6000);
+
+    notification.on('touchstart', function() {
+        notification.addClass('close');
+        setTimeout(function() {
+            notification.remove();
+        }, 300 );
+    });
+
 }
 window.onload = function() {
 	//Set the time field when the page loads.
